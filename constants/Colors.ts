eb857ad3161304134a9248a0cad677bbf1d045d3
khalifahnur <khalifahnur1095@gmail.com>
2024-08-31 18:56:29 +0300

    added constant color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,8 +8,8 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#999',
+    background: '#F2F4F7',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
