02e47911dbbeae7976f1177390d05f12d312ea04
khalifahnur <khalifahnur1095@gmail.com>
2024-09-02 14:17:56 +0300

    added menu screen to the stack

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -7,28 +7,32 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import ManageProduct from '@/components/Add/ManageProduct'
 import { Ionicons } from '@expo/vector-icons'
+import HeaderComponent from '@/components/Add/HeaderComponent'
+import { useRouter } from 'expo-router'
 
 type AddScreenProps = {
   onClose: () => void;
 };
 
 export default function AddScreen({onClose}:AddScreenProps) {
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor='black' />
       <ThemedView
-        style={[styles.container, {marginBottom:65}]}
+        style={styles.container}
         lightColor="#F2F4F7"
       >
         <View style={styles.header}>
           <View>
             <Text style={styles.HeaderTxt}>Manage Products</Text>
           </View>
-          <Pressable style={styles.headerBtn}>
+          <Pressable style={styles.headerBtn} onPress={()=>router.push('screens/addSection')}>
             <Ionicons name="add-sharp" size={20} color="#fff" />
             <Text style={{color:'#fff'}}>Add New Product</Text>
           </Pressable>
         </View>
+        <HeaderComponent />
         {/* <AddContainer /> */}
         <ManageProduct />
 
@@ -46,6 +50,7 @@ const styles = StyleSheet.create({
   header:{
     paddingHorizontal:20,
     marginTop:10,
+    marginBottom:10,
     flexDirection:'row',
     alignItems:'center',
     justifyContent:'space-between'
