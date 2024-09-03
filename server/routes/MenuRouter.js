e6354e8176b70ea2cc24efec58aecae7384de249
khalifahnur <khalifahnur1095@gmail.com>
2024-09-03 17:27:26 +0300

    added fetchMenu to router

--- server/routes/MenuRouter.js
+++ server/routes/MenuRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const MenuController = require("../controllers/MenuController");
+const FetchMenuController  = require("../controllers/DataController");
 
 router.post("/createMenu", MenuController.createMenu);
+router.get("/fetchMenu",FetchMenuController.FetchMenu);
 
 
 module.exports = router;
