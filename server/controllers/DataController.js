dbecb48bd6b4a81930433de4c29a222118704ce8
khalifahnur <khalifahnur1095@gmail.com>
2024-09-03 17:24:28 +0300

    fecth data/menu from db

---
+++ server/controllers/DataController.js
@@ -0,0 +1,14 @@
+const Menu = require("../models/Menu")
+
+const FetchMenu = async (req, res) => {
+  const { restaurantId } = req.query;
+
+  try {
+    const data = await Menu.find({ restaurantId: restaurantId });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+};
+
+module.exports= {FetchMenu};
