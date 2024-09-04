d059a91df87a75b01e3f7a35ffa3b4f33fd91c68
khalifahnur <khalifahnur1095@gmail.com>
2024-09-04 20:53:49 +0300

    fix get data from query

--- server/controllers/DataController.js
+++ server/controllers/DataController.js
@@ -1,11 +1,11 @@
 const Menu = require("../models/Menu")
 
 const FetchMenu = async (req, res) => {
-  const { restaurantId } = req.body;
+  const { restaurantId } = req.query;
 
   try {
     const data = await Menu.find({ restaurantId: restaurantId });
-    res.json(data);
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch data" });
   }
