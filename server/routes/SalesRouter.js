b4bab48adab6abb1e0d15114a7e5f0302aa79caf
khalifahnur <khalifahnur1095@gmail.com>
2024-09-06 20:23:05 +0300

    fix route name

--- server/routes/SalesRouter.js
+++ server/routes/SalesRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const SaleController = require("../controllers/SalesController");
 
 router.post("/createSales", SaleController.SalesController);
+router.get("/fetchSales",SaleController.FetchSales)
 
 module.exports = router;
