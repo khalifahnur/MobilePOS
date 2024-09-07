2e6b7efb29f630a2c41fc1c71e644bb11c8f159d
khalifahnur <khalifahnur1095@gmail.com>
2024-09-07 18:10:07 +0300

    changed style for headerTitle

--- components/History/HeaderContentTitle.tsx
+++ components/History/HeaderContentTitle.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginTop: 5,
-    marginBottom:20,
+    
     padding:18,
   },
   HeaderText: {
