dc09cad5585be168db5b53d532c139229d382df6
khalifahnur <khalifahnur1095@gmail.com>
2024-09-08 21:35:16 +0300

    ref: handle filtered data properly

--- app/screens/filter.tsx
+++ app/screens/filter.tsx
@@ -6,70 +6,17 @@ import {
   View,
   Pressable,
   ScrollView,
+  FlatList,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { Calendar } from "react-native-calendars";
-import Slider from "@react-native-community/slider";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
+
 
 export default function FilterScreen() {
-  const [selectedDates, setSelectedDates] = useState({});
-  const [selectedTimePeriod, setSelectedTimePeriod] = useState("AM");
-  const [selectedTime, setSelectedTime] = useState('');
-  //const [selectedAM, setSelectedAM] = useState('');
-  const [selectedCost, setSelectedCost] = useState(350);
-
-  // console.log(Object.keys(selectedDates));
-  // console.log(selectedPM);
-  // console.log("cost", selectedCost);
-  const FilteredDates = Object.keys(selectedDates);
-
-  const AM = [
-    { id: 1, am: "00-03 hrs" },
-    { id: 2, am: "03-06 hrs" },
-    { id: 3, am: "06-09 hrs" },
-    { id: 4, am: "09-12 hrs" },
-  ];
-  const PM = [
-    { id: 1, pm: "12-15 hrs" },
-    { id: 2, pm: "15-18 hrs" },
-    { id: 3, pm: "18-21 hrs" },
-    { id: 4, pm: "21-00 hrs" },
-  ];
-
   const router = useRouter();
-
-  const handleDayPress = (day) => {
-    setSelectedDates((prevDates) => ({
-      ...prevDates,
-      [day.dateString]: {
-        selected: true,
-        disableTouchEvent: true,
-        selectedDotColor: "orange",
-      },
-    }));
-  };
-
-  const handleReset = () => {
-    setSelectedDates({});
-    setSelectedTimePeriod("AM");
-    setSelectedTime("");
-  };
-
-
-  const handleFilter = () => {
-    if (FilteredDates && selectedCost && selectedTime) {
-      router.push({
-        pathname: '/history',
-        params: {
-          FilteredDates: JSON.stringify(FilteredDates),
-          selectedCost: selectedCost.toString(),
-          selectedTime:selectedTime,
-        }
-      });
-    }
-  };
-  
+  const params = useLocalSearchParams();
+  const filteredData = params;
+  console.log("filtered screen",filteredData)
   
 
   return (
@@ -85,118 +32,46 @@ export default function FilterScreen() {
               <AntDesign name="close" size={20} color="black" />
             </TouchableOpacity>
           </View>
-
-          {/* Time */}
-
-          <View style={styles.card}>
-            <Text style={styles.subHeaderText}>Month</Text>
-            <Calendar  onDayPress={handleDayPress} markedDates={selectedDates} />
-          </View>
-
-          {/* Time */}
-
-          <View style={styles.card}>
-            <Text style={styles.subHeaderText}>Time</Text>
-            <View style={styles.timeContainer}>
-              <Pressable
-                style={[
-                  styles.timeButton,
-                  selectedTimePeriod === "AM" && styles.timeButtonSelected,
-                ]}
-                onPress={() => setSelectedTimePeriod("AM")}
-              >
-                <Text
-                  style={selectedTimePeriod === "AM" && styles.selectedText}
-                >
-                  AM
-                </Text>
-              </Pressable>
-              <Pressable
-                style={[
-                  styles.timeButton,
-                  selectedTimePeriod === "PM" && styles.timeButtonSelected,
-                ]}
-                onPress={() => setSelectedTimePeriod("PM")}
-              >
-                <Text
-                  style={selectedTimePeriod === "PM" && styles.selectedText}
-                >
-                  PM
-                </Text>
-              </Pressable>
-            </View>
-            {selectedTimePeriod === "AM" ? (
-              <View>
-                {AM.map(({ am, id }) => (
-                  <View key={id}>
-                    <TouchableOpacity
-                      style={{ padding: 5 }}
-                      onPress={() => setSelectedTime(am)}
-                    >
-                      <Text
-                        style={{
-                          backgroundColor: selectedTime === am ? "#4d81f1" : "#84d76b",
-                          padding: 10,
-                          fontSize: 15,
-                          borderRadius: 8,
-                          color: selectedTime === am ? "#fff" : "#1e1e1e",
-                        }}
-                      >
-                        {am}
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                ))}
-              </View>
-            ) : (
-              <View>
-                {PM.map(({ pm, id }) => (
-                  <View key={id}>
-                    <TouchableOpacity
-                      style={{ padding: 5 }}
-                      onPress={() => setSelectedTime(pm)}
-                    >
-                      <Text
-                        style={{
-                          backgroundColor: selectedTime === pm ? "#4d81f1" : "#84d76b",
-                          padding: 10,
-                          fontSize: 15,
-                          borderRadius: 8,
-                          color: selectedTime === pm ? "#fff" : "#1e1e1e",
-                        }}
-                      >
-                        {pm}
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                ))}
-              </View>
-            )}
-          </View>
-
-          <View style={styles.card}>
-            <Text style={styles.subHeaderText}>Cost</Text>
-            <Slider
-              style={styles.slider}
-              minimumValue={350}
-              maximumValue={12000}
-              lowerLimit={350}
-              minimumTrackTintColor="#1e1e1e"
-              maximumTrackTintColor="#000000"
-              value={selectedCost} // initial value
-              onValueChange={(value) => setSelectedCost(value)}
-            />
-          </View>
         </View>
+         
       </ScrollView>
-      <View style={styles.footer}>
-        <Pressable style={styles.footerButton} onPress={handleReset}>
-          <Text style={styles.footerButtonText}>Reset</Text>
-        </Pressable>
-        <Pressable style={styles.footerButton} onPress={handleFilter}>
-          <Text style={styles.footerButtonText}>Apply Filters</Text>
-        </Pressable>
-      </View>
+    <FlatList
+  data={filteredData}
+  renderItem={({ item, index }) => {
+    const createdAtDate = new Date(item?.createdAt);
+    return (
+      <>
+        <View key={item?._id} style={{ paddingHorizontal: 20 }}>
+          <TouchableOpacity
+          //onPress={()=>HandleModalDetails(item)}
+            style={[
+              styles.Content,
+              {
+                backgroundColor: index % 2 === 0 ? "#cce9d5" : "#ffff",
+                borderRadius: 8,
+              },
+            ]}
+          >
+            {/* Check if data is valid */}
+            <Text style={styles.ContentText}># {item?._id.slice(-4,-1)} </Text>
+            <Text style={styles.ContentText}>
+              {/* Ensure valid date */}
+              {isNaN(createdAtDate.getTime())
+                ? "Invalid Date"
+                : createdAtDate.toLocaleString()}
+            </Text>
+            <Text style={styles.ContentText}>{item?.totalCost}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.Divider} />
+      </>
+    );
+  }}
+
+  
+  showsVerticalScrollIndicator={false}
+/>
+      
     </>
   );
 }
@@ -283,5 +158,23 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 16,
     fontWeight: "600",
+  },
+  Content: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  ContentText: {
+    textAlign: "center",
+    padding: 5,
+    fontSize: 15,
+  },
+  Divider: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#d4d4d4",
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
