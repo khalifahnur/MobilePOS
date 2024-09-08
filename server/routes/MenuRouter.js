c77b9835dd61920407ff0dc841fc69f7381a3b29
khalifahnur <khalifahnur1095@gmail.com>
2024-09-08 21:29:56 +0300

    added update and remove route

--- server/routes/MenuRouter.js
+++ server/routes/MenuRouter.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const MenuController = require("../controllers/MenuController");
 const FetchMenuController  = require("../controllers/DataController");
+const RemoveController = require("../controllers/RemoveItemController");
+const UpdateController = require("../controllers/UpdateItemController")
 
 router.post("/createMenu", MenuController.createMenu);
 router.get("/fetchMenu",FetchMenuController.FetchMenu);
+router.post("/remove",RemoveController.removeItem);
+router.post("/update",UpdateController.updateItem);
 
 
 module.exports = router;
