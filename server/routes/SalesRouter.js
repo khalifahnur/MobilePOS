34f2d1f89b861e3ea6ad7932ef7d5061e44304d5
khalifahnur <khalifahnur1095@gmail.com>
2024-09-08 09:26:18 +0300

    added filter routes

--- server/routes/SalesRouter.js
+++ server/routes/SalesRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const SaleController = require("../controllers/SalesController");
+const FilterController = require("../controllers/FilterController");
 
 router.post("/createSales", SaleController.SalesController);
-router.get("/fetchSales",SaleController.FetchSales)
+router.get("/fetchSales",SaleController.FetchSales);
+router.get("/filteredSales",FilterController.FilteredSales);
 
 module.exports = router;
