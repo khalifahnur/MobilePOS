5643fe7d384233d9012fd44875e653ffc710cb76
khalifahnur <khalifahnur1095@gmail.com>
2024-09-10 10:17:44 +0300

    ref: http method from post to delete

--- server/controllers/RemoveItemController.js
+++ server/controllers/RemoveItemController.js
@@ -1,29 +1,29 @@
 const menu = require("../models/Menu");
 
 const removeItem = async (req, res) => {
-  const { restaurantId, title, itemId } = req.body; 
 
+  const { id, restaurantId, title } = req.body;
+  console.log(req.body)
   try {
     const result = await menu.updateOne(
-      {
-        restaurantId: restaurantId,
-        "data.title": title,
-      },
+      { restaurantId: restaurantId, 'data.title': title },
       {
         $pull: {
-          "data.$.description": { _id: itemId },
+          data: {
+            _id: id,
+          },
         },
       }
     );
 
-    if (result.nModified === 0) {
-      return res.status(404).json({ message: "Item not found or already removed" });
+    if (result.modifiedCount > 0) {
+      res.status(200).json({ message: 'Item deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Item not found' });
     }
-
-    res.status(200).json({ message: "Item removed successfully", result });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: 'An error occurred', error });
   }
 };
 
-module.exports = { removeItem};
+module.exports = { removeItem };
