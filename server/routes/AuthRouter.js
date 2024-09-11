a16389473b8e97113d0b6e658267f53f70e469d7
khalifahnur <khalifahnur1095@gmail.com>
2024-09-11 22:07:47 +0300

    added forgot-password route

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/AuthController");
+const forgotPsswdController = require("../controllers/ForgotPsswdController");
 
 router.post("/SignUp", AuthController.signUpUser);
 router.post("/SignIn", AuthController.loginUser);
-router.post("/:userId/updateRestaurantDetails", AuthController.updateRestaurantDetails)
+router.post("/:userId/updateRestaurantDetails", AuthController.updateRestaurantDetails);
+router.post("/forgot-password",forgotPsswdController.forgotPsswd);
 
 module.exports = router;
