6a15cd9166fa01e67f90ebfb8fc33170dc04fbd4
khalifahnur <khalifahnur1095@gmail.com>
2024-09-11 22:21:18 +0300

    added verify-code route

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 const forgotPsswdController = require("../controllers/ForgotPsswdController");
+const verifyCodeController = require("../controllers/VerifyCodeController");
 
 router.post("/SignUp", AuthController.signUpUser);
 router.post("/SignIn", AuthController.loginUser);
 router.post("/:userId/updateRestaurantDetails", AuthController.updateRestaurantDetails);
 router.post("/forgot-password",forgotPsswdController.forgotPsswd);
+router.post("/verify-code",verifyCodeController.verifyCode);
 
 module.exports = router;
