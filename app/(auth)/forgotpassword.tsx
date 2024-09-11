223d0ebfb5a3fb540c0f42fec80780e01e568ac2
khalifahnur <khalifahnur1095@gmail.com>
2024-09-11 22:05:06 +0300

    handler forgot password route

--- app/(auth)/forgotpassword.tsx
+++ app/(auth)/forgotpassword.tsx
@@ -1,18 +1,40 @@
 import {
+    Alert,
   Pressable,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import InputField from "@/components/InputField";
 import { EvilIcons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
+import axios from "axios";
+import Constants from "expo-constants";
 
 export default function ForgotScreen() {
+    const [email,setEmail] = useState('');
     const router = useRouter();
+    const localhost = Constants.expoConfig?.extra?.localhost;
+
+    const HandleForgotPassword = async()=>{
+        if(email){
+            try{
+                const response = await axios.post(`http://${localhost}:3002/api/auth/forgot-password`,{email});
+                if(response.status === 200){
+                    router.push({pathname:"/(auth)/codeverify",params:{email}})
+                }else if (response.status === 404){
+                    Alert.alert("Email not found");
+                }
+            }catch(e){
+                console.log("Error occured",e);
+                Alert.alert("Error sending verification code")
+            }
+        }
+        
+    }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -23,10 +45,10 @@ export default function ForgotScreen() {
       <View style={styles.subContainer}>
         <Text style={styles.headertxt}>Forgot password?</Text>
         <View style={styles.textinp}>
-          <InputField labelTxt="Email :" type="email" />
+          <InputField labelTxt="Email :" type="email" onChangeText={(txt)=>setEmail(txt)} />
         </View>
 
-          <Pressable style={styles.resetbtn} onPress={()=>router.push("/(auth)/codeverify")}>
+          <Pressable style={styles.resetbtn} onPress={HandleForgotPassword}>
             <Text style={{color:'#fff'}}>Reset Password</Text>
           </Pressable>
 
