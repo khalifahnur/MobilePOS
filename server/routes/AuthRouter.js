c03676f5704ece27e9a375fb3a74d5523a98bc03
khalifahnur <khalifahnur1095@gmail.com>
2024-09-11 22:51:15 +0300

    added reset-password route

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 const forgotPsswdController = require("../controllers/ForgotPsswdController");
 const verifyCodeController = require("../controllers/VerifyCodeController");
+const resetPasswordController = require("../controllers/NewPassword")
 
 router.post("/SignUp", AuthController.signUpUser);
 router.post("/SignIn", AuthController.loginUser);
 router.post("/:userId/updateRestaurantDetails", AuthController.updateRestaurantDetails);
 router.post("/forgot-password",forgotPsswdController.forgotPsswd);
 router.post("/verify-code",verifyCodeController.verifyCode);
+router.post("/reset-password",resetPasswordController.resetPassword);
 
 module.exports = router;
